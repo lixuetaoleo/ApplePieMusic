b00b0fe161ed4c68b28db1674b72fc8de607ae8c
lixuetao <lixuetaoleo@163.com>
2020-11-09 20:38:24 +0800

    增加webpack的配置项

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,21 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  mode: 'development',
   entry: './src/index.jsx',
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
 };
