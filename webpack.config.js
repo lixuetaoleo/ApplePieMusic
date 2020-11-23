6c29f0a2edda5e548c34f0eba2cd273a912c63c8
lixuetao <lixuetaoleo@163.com>
2020-11-23 18:33:16 +0800

    webpack修改entry和output的文件名

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.jsx',
+  entry: {
+    app: './src/index.jsx',
+  },
   output: {
-    filename: 'main.bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'build'),
   },
   resolve: {
