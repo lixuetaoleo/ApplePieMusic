94e852598e3ec6e41be3c43627b6427972f90655
lixuetao <lixuetaoleo@163.com>
2020-12-07 23:23:37 +0800

    增加路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import './index.less';
+import { Switch, Route, Link } from 'react-router-dom';
+import Recommend from './pages/Recommend';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import NavBar from './components/NavBar';
 
-export default function() {
-  return(
-    <div className="body">
-      hello apple pie music!
+export default function () {
+  return (
+    <div>
+      <Switch>
+        <Route path='/recommend'>
+          <Recommend />
+        </Route>
+        <Route path='/search'>
+          <Search />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+      <NavBar />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function() {
+  return (
+    <div>
+      home,资料库
+    </div>
+  );
+}
